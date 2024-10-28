90977bcd3a83430096b27c27c5d827badaaa84f1
Majer11 <161945724+Majer11@users.noreply.github.com>
2024-10-28 15:35:47 +0200

    Update homework.py

--- 1_lesson/homework.py
+++ 1_lesson/homework.py
@@ -1,10 +1,17 @@
 """
+Ask the user for their age and then calculate and print their age in dog's years.
+"""
+user_age = int(input("Please enter your age:"))
+dog_years = user_age * 7
+print(f"Your are {dog_years} in dog years")
+
+
+
+"""
 Get a number from the user, check and print that the number is positive, 
 negative, or zero
 """
-
 num = float(input())
-
 if num > 0:
     print("Number is positive")
 elif num < 0:
