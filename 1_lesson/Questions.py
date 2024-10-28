5dee7e271ea80b11b4ab0aeb64980fb5be99eb65
Majer11 <161945724+Majer11@users.noreply.github.com>
2024-10-28 15:40:43 +0200

    Update Questions.py

--- 1_lesson/Questions.py
+++ 1_lesson/Questions.py
@@ -1,1 +1,1 @@
-How to calculate a number. Example: 1.79456781 to be calculated and ends in the 4th number after dot.
+How to calculate a number. Example: 1.79456781 to be calculated and ends in the 4th number after dot. I want to be printed 1.7946
