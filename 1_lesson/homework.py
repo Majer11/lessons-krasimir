d6387f50804e56dd104ace290e9ff32173593c8f
Majer11 <161945724+Majer11@users.noreply.github.com>
2024-10-28 15:38:56 +0200

    Update homework.py

--- 1_lesson/homework.py
+++ 1_lesson/homework.py
@@ -5,7 +5,16 @@ user_age = int(input("Please enter your age:"))
 dog_years = user_age * 7
 print(f"Your are {dog_years} in dog years")
 
+"""
+Write a program that gets 2 numbers from the user for the sides of a rectangle,
+calculates its area P (perimeter) and prints the result to the console/terminal.
+"""
+num1 = float(input())
+num2 = float(input())
 
+perimeter = num1 * num2
+
+print(perimeter)
 
 """
 Get a number from the user, check and print that the number is positive, 
