22fbdeff4d804c762154fe5a291280192f57e185
Majer11 <161945724+Majer11@users.noreply.github.com>
2024-10-31 14:24:43 +0200

    Create Arithmetic_operations.py

---
+++ 1_lesson/Arithmetic_operations.py
@@ -0,0 +1,27 @@
+a = 5
+b = 3
+
+a + b
+
+a - b
+
+a * b
+
+# division operator (/) - performs true division, resulting in a float-point number
+print(a / b)
+
+# // - you get a whole number from the division
+print("// operator:", a // b)
+
+base = 2
+exponent = 3
+result = base ** exponent
+
+# remainder
+result = a % b
+print("Remainder", result)
+
+# How to calculate a number. Example: 1.79456781 to be calculated and ends in the 4th number after dot. I want to be printed 1.7946
+
+x = 1.79456781
+print(f"Output: {x:.0f}")
